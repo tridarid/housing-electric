1014ef2daa444854a3d59c069ca784af3ba2bde2
tridianto <tridianto.putra@gmail.com>
2016-10-26 18:02:46 +0700

    create dashboard

---
+++ HousingElectric/app/src/main/java/tridarid/housingelectric/activity/dashboard.java
@@ -0,0 +1,15 @@
+package tridarid.housingelectric.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import tridarid.housingelectric.R;
+
+public class dashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+    }
+}
